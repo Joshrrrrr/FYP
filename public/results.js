e742bca9714bf2dd426635b6c7c3e79aee2a1715
Joshrrrrr <C19303023@mytudublin.ie>
2023-03-14 15:43:53 +0000

    Fixing searchType on results page being sent to server

--- public/results.js
+++ public/results.js
@@ -12,9 +12,24 @@ const h4text1 = document.getElementById('h4text1');
 const h4text2 = document.getElementById('h4text2');
 const sliceItems = document.querySelectorAll('.slice');
 const searchTypeCheckboxes = document.querySelectorAll('input[name="searchType"]');
+const form = document.getElementById("myForm");
 
 searchTypeCheckboxes.forEach(checkbox => {
   checkbox.addEventListener('click', () => {
+    // create or update hidden input field with checkbox value
+    let input = document.getElementById("hidden_" + checkbox.id);
+    if (!input) {
+      input = document.createElement("input");
+      input.type = "hidden";
+      input.id = "hidden_" + checkbox.id;
+      input.name = checkbox.name;
+      form.appendChild(input);
+    }
+    if (checkbox.checked) {
+      input.value = checkbox.value;
+    } else {
+      input.value = "";
+    }
     // Uncheck all other checkboxes
     searchTypeCheckboxes.forEach(otherCheckbox => {
       if (otherCheckbox !== checkbox) {
